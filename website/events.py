4f4aedeb77de301d48c0647026da2dffa4fb7d14
Icey00 <henislamdagan@gmail.com>
2021-10-12 19:13:23 +1000

    edited models.py and events.py

--- website/events.py
+++ website/events.py
@@ -1,9 +1,13 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask_login.utils import login_required, current_user
 from werkzeug.utils import secure_filename
 from . import db
 from datetime import date, datetime
 import os
 from werkzeug.utils import secure_filename
+from .forms import CommentForm, EventForm
+from .models import Event, Comment
+
 
 # from .models import Destination, Comment
 # from .forms import DestinationForm, CommentForm
@@ -11,22 +15,25 @@ from werkzeug.utils import secure_filename
 bp = Blueprint('event', __name__, url_prefix='/events')
 
 # route for events based on the ID provided form the URL
-@bp.route('/<int:id>')
+@bp.route('/<id>')
 def show(id):
-    # destination = Destination.query.filter_by(id=id).first()
+
+    event = Event.query.filter_by(id=id).first()
     # # create the comment form
-    # comment_form = CommentForm()    
+    comment_form = CommentForm()    
 
     # # error handling 
-    # if destination is None:
-    #   flash(f"Cound not find a destination!", "warning")
-    #   return redirect(url_for("main.index"))
-    # return render_template('events/show.html', destination=destination, form=comment_form, id=id)
+    if Event is None:
+       flash(f"Cound not find a destination!", "warning")
+       return redirect(url_for("main.index"))
+    
+    return render_template('events/show.html', event=event, form=comment_form, id=id)
 
-    return render_template('events/show.html')
+  
 
 # route to the event create page
 @bp.route('/create', methods = ['GET', 'POST'])
+@login_required
 def create():
 #   form = DestinationForm()
 #   if form.validate_on_submit():
@@ -48,29 +55,29 @@ def create():
   return render_template('events/create.html')
 
 # route for posting comment to the event based on the given ID
-@bp.route('/<int:id>/comment', methods = ['GET', 'POST'])  
-# def comment(id):  
-#     form = CommentForm()  
-#     if form.validate_on_submit():
-#       # POST
-#       comment = Comment(
-#         text = form.text.data,
-#         user_id = 1,
-#         destination_id = id,
-#         created_at = datetime.now()
-#       )
+@bp.route('/<event>/comment', methods = ['GET', 'POST'])
+@login_required  
+def comment(event):  
 
-#       db.session.add(comment) 
-#       db.session.commit()
+  comment_form = CommentForm()  
+  event_obj = Event.query.filter_by(id=event).first()
+  if comment_form.validate_on_submit():
+
+      # POST
+      comment = Comment(text = comment_form.text.data,
+                        user = current_user,
+                        event_id = event_obj,
+                        created_at = datetime.now())
+
+      db.session.add(comment) 
+      db.session.commit()
       
-#       # save to database
-#       flash(f"Comments successfully posted","success")
-
-#     # GET
-#     return redirect(url_for("destination.show", id=id))
+      # save to database
+      flash(f"Comments successfully posted","success")
+    # GET
+      return redirect(url_for("events.show", id=event))
 
 @bp.route("<int:id>/update", methods = ["GET", "POST"])
-
 def check_upload_file(form):
   fp = form.image.data
   filename = fp.filename
