0d50e8d95748a115e78d391d28ad15ba4873a750
benjaminhooqut <benhoo12@hotmail.com>
2021-10-16 15:19:55 +1000

    edited route to create page, double check for me

--- website/events.py
+++ website/events.py
@@ -14,78 +14,91 @@ from .models import Event, Comment
 
 bp = Blueprint('event', __name__, url_prefix='/events')
 
-EVENT_GENRES = ["Country", "Electronic", "Funk", "Hip Hop", "Jazz", "House", "Pop", "Rap", "Rock"]
+EVENT_GENRES = ["Country", "Electronic", "Funk",
+                "Hip Hop", "Jazz", "House", "Pop", "Rap", "Rock"]
 
 # route for events based on the ID provided form the URL
+
+
 @bp.route('/<id>')
 def show(id):
 
     #event = Event.query.filter_by(id=id).first()
     # create the comment form
-    comment_form = CommentForm()    
+    comment_form = CommentForm()
 
-    # # error handling 
+    # # error handling
     if Event is None:
-       flash(f"Cound not find a destination!", "warning")
-       return redirect(url_for("main.index"))
-    
-    #return render_template('events/show.html', event=event, form=comment_form, id=id)
+        flash(f"Cound not find a destination!", "warning")
+        return redirect(url_for("main.index"))
+
+    # return render_template('events/show.html', event=event, form=comment_form, id=id)
     return render_template('events/show.html', form=comment_form)
 
-  
 
 # route to the event create page
-@bp.route('/create', methods = ['GET', 'POST'])
+@bp.route('/create', methods=['GET', 'POST'])
 # @login_required
 def create():
-  form = EventForm()
+    form = EventForm()
 #   if form.validate_on_submit():
 #     destination = Destination(
-#       name = form.name.data,
-#       description = form.description.data,
+#       name = event_form.event_name.data,
+#       artist_name = event_form.artist_name.data,
+#       status = event_form.status.data,
+#       genre = event_form.genre.data,
+#       date = event_form.datetime.data,
+#       location = event_form.location.data,
+#       map = event_form.google_map.data,
+#       description = event_form.description.data,
 #       image = check_upload_file(form),
-#       currency = form.currency.data
-#     )    
+#       price = event_form.price.data,
+#       ticket_count = event_form.ticket_count.data
+#     )
 #     # add the object to the db session
-#     db.session.add(destination)
+#     db.session.add(event)
 #     # commit to the database
 #     db.session.commit()
 
-#     flash(f'Successfully created new travel destination', 'success')
+#     flash(f'Successfully created new event', 'success')
 
-#     return redirect(url_for('destination.show', id=destination.id)) 
+#     return redirect(url_for('destination.show', id=destination.id))
 #   return render_template('events/create.html', form=form)
-  return render_template('events/create.html', form = form, heading="Create Event")
+    return render_template('events/create.html', form=form, heading="Create Event")
 
 # route for posting comment to the event based on the given ID
-@bp.route('/<event>/comment', methods = ['GET', 'POST'])
-@login_required  
-def comment(event):  
 
-  comment_form = CommentForm()  
-  event_obj = Event.query.filter_by(id=event).first()
-  if comment_form.validate_on_submit():
 
-      # POST
-      comment = Comment(text = comment_form.text.data,
-                        user = current_user,
-                        event_id = event_obj,
-                        created_at = datetime.now())
+@bp.route('/<event>/comment', methods=['GET', 'POST'])
+@login_required
+def comment(event):
 
-      db.session.add(comment) 
-      db.session.commit()
-      
-      # save to database
-      flash(f"Comments successfully posted","success")
+    comment_form = CommentForm()
+    event_obj = Event.query.filter_by(id=event).first()
+    if comment_form.validate_on_submit():
+
+        # POST
+        comment = Comment(text=comment_form.text.data,
+                          user=current_user,
+                          event_id=event_obj,
+                          created_at=datetime.now())
+
+        db.session.add(comment)
+        db.session.commit()
+
+        # save to database
+        flash(f"Comments successfully posted", "success")
     # GET
-      return redirect(url_for("events.show", id=event))
+        return redirect(url_for("events.show", id=event))
 
-@bp.route("<int:id>/update", methods = ["GET", "POST"])
+
+@bp.route("<int:id>/update", methods=["GET", "POST"])
 def check_upload_file(form):
-  fp = form.image.data
-  filename = fp.filename
-  BASE_PATH = os.path.dirname(__file__)
-  upload_path = os.path.join(BASE_PATH, "static/images/", secure_filename(filename))
-  db_upload_path="static/images/"+secure_filename(filename)
-  fp.save(upload_path)
-  return db_upload_path
+    fp = form.image.data
+    filename = fp.filename
+    BASE_PATH = os.path.dirname(__file__)
+    upload_path = os.path.join(
+        BASE_PATH, "static/images/", secure_filename(filename))
+    db_upload_path = "static/images/"+secure_filename(filename)
+    fp.save(upload_path)
+    return db_upload_path
