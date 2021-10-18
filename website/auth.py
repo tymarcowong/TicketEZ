9bb3b513349a762d1560a06c3badfb68e804c32f
Icey00 <henislamdagan@gmail.com>
2021-10-18 15:37:32 +1000

    ..

--- website/auth.py
+++ website/auth.py
@@ -90,30 +90,6 @@ def register():
     # return render_template("user.html", heading="Register", form=register_form)
 
 
-    @bp.route('/login', methods=['GET', 'POST'])
-def authenticate(): #view function
-    print('In Login View function')
-    login_form = LoginForm()
-    error=None
-    if(login_form.validate_on_submit()==True):
-        user_name = login_form.user_name.data
-        password = login_form.password.data
-        u1 = User.query.filter_by(name=user_name).first()
-        if u1 is None:
-            error='Incorrect user name'
-        elif not check_password_hash(u1.password_hash, password): # takes the hash and password
-            error='Incorrect password'
-        if error is None:
-            login_user(u1)
-            nextp = request.args.get('next') #this gives the url from where the login page was accessed
-            print(nextp)
-            if next is None or not nextp.startswith('/'):
-                return redirect(url_for('index'))
-            return redirect(nextp)
-        else:
-            flash(error)
-    return render_template('user.html', form=login_form, heading='Login')
-
 
 @bp.route('/logout', methods=["POST", "GET"])
 @login_required
