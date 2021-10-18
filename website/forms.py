017abc05349711c55b33c5ed5fff5769893aac9b
benjaminhooqut <benhoo12@hotmail.com>
2021-10-18 14:57:10 +1000

    booking form made

--- website/forms.py
+++ website/forms.py
@@ -7,6 +7,8 @@ from flask_wtf.file import FileRequired, FileField, FileAllowed
 ALLOWED_FILES = ["PNG", "JPG", "png", "jpg", "JPEG", "jpeg"]
 
 # creates the login information
+
+
 class LoginForm(FlaskForm):
     user_name = StringField("User Name", validators=[
                             InputRequired('Enter user name')])
@@ -17,28 +19,46 @@ class LoginForm(FlaskForm):
 # registration form
 class RegisterForm(FlaskForm):
     user_name = StringField("User Name", validators=[InputRequired()])
-    email_id = StringField("Email Address", validators=[InputRequired(), Email("Please enter a valid email")])
-    contact = IntegerField("Contact Number", validators=[InputRequired('Please enter a valid contact number')])
-    address = StringField("Address", validators=[InputRequired('Please enter your address')])
-    password = PasswordField("Password", validators=[InputRequired(), EqualTo('confirm', message="Passwords should match")])
+    email_id = StringField("Email Address", validators=[
+                           InputRequired(), Email("Please enter a valid email")])
+    contact = IntegerField("Contact Number", validators=[
+                           InputRequired('Please enter a valid contact number')])
+    address = StringField("Address", validators=[
+                          InputRequired('Please enter your address')])
+    password = PasswordField("Password", validators=[InputRequired(), EqualTo(
+        'confirm', message="Passwords should match")])
     confirm = PasswordField("Confirm Password", validators=[InputRequired()])
 
 # create event form
+
+
 class EventForm(FlaskForm):
     event_name = StringField('Event Name', validators=[InputRequired()])
     artist_name = StringField('Artist Name', validators=[InputRequired()])
-    status = SelectField("Status", choices=[("", "--Please select status--"), ("active", "Active"), ("upcoming", "Upcoming"), ("inactive", "Inactive")])
-    genre = SelectField("Genre", choices=[("", "--Please select genre--"), ("country", "Country"), ("electronic", "Electronic"), ("funk", "Funk"), ("hiphop", "Hip Hop"), ("jazz", "Jazz"), ("house", "House"), ("pop", "Pop"), ("rap", "Rap"), ("rock", "Rock")])
+    status = SelectField("Status", choices=[("", "--Please select status--"),
+                                            ("active", "Active"), ("upcoming", "Upcoming"), ("inactive", "Inactive")])
+    genre = SelectField("Genre", choices=[("", "--Please select genre--"), ("country", "Country"), ("electronic", "Electronic"),
+                                          ("funk", "Funk"), ("hiphop", "Hip Hop"), ("jazz", "Jazz"), ("house", "House"), ("pop", "Pop"), ("rap", "Rap"), ("rock", "Rock")])
     datetime = StringField('Date and Time', validators=[InputRequired()])
     location = StringField('Location', validators=[InputRequired()])
     google_map = StringField('Google Map Link', validators=[InputRequired()])
     description = TextAreaField('Description', validators=[InputRequired()])
-    image = FileField('Event Image', validators=[ FileRequired(), FileAllowed(ALLOWED_FILES, message=f"Accepted file types: {ALLOWED_FILES}")])
+    image = FileField('Event Image', validators=[FileRequired(), FileAllowed(
+        ALLOWED_FILES, message=f"Accepted file types: {ALLOWED_FILES}")])
     price = StringField('Price', validators=[InputRequired()])
-    num_tickers = IntegerField("Number of Tickets", validators=[InputRequired()])
+    num_tickers = IntegerField(
+        "Number of Tickets", validators=[InputRequired()])
     submit = SubmitField()
 
 # comment form
+
+
 class CommentForm(FlaskForm):
     text = TextAreaField('Comment', validators=[InputRequired()])
     submit = SubmitField('Create')
+
+
+class BookingForm(FlaskForm):
+    num_tickets = IntegerField(
+        'Number of Tickets', validators=[InputRequired()])
+    submit = SubmitField()
